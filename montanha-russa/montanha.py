b87a666513f91271bd7dc53756e77428982c538f
RodrigoNdeC <rodrigondec@gmail.com>
2017-05-22 19:49:12 -0300

    adicionado logging e erro

--- montanha-russa/montanha.py
+++ montanha-russa/montanha.py
@@ -3,19 +3,27 @@ from random import randrange
 from threading import Thread, Event
 
 
-def car():
-    pass
+def setup_logger(logger_name, log_file, level=logging.INFO):
+    l = logging.getLogger(logger_name)
+    formatter = logging.Formatter('%(message)s')
+    fileHandler = logging.FileHandler(log_file, mode='w')
+    fileHandler.setFormatter(formatter)
+    streamHandler = logging.StreamHandler()
+    streamHandler.setFormatter(formatter)
+
+    l.setLevel(level)
+    l.addHandler(fileHandler)
+    # l.addHandler(streamHandler)
 
 
-def passenger():
-    pass
+setup_logger("log", "info.log")
+log = logging.getLogger("log")
 
+def print_log(msg):
+    print(msg)
+    log.info(msg)
 
-threads = []
+class Erro(Exception):
+    def __init__(self, msg):
+        self.msg = msg
 
-t = Thread(name='car', target=car, args=('test',))
-threads.append(t)
-t.start()
-
-for thread in threads:
-    thread.join()
