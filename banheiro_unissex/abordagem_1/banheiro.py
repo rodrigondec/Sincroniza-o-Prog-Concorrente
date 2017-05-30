dd1b005bf49f56d810321284006706e007182d58
RodrigoNdeC <rodrigondec@gmail.com>
2017-05-29 23:03:50 -0300

    banheiro e pessoa com fila e controlador de fila

--- banheiro_unissex/abordagem_1/banheiro.py
+++ banheiro_unissex/abordagem_1/banheiro.py
@@ -55,93 +55,160 @@ def print_pessoas_log(msg):
 
 class Banheiro(object):
     """Implementação de um banheiro"""
-# TODO documentar variáveis
+
+    id_banheiro = 1
 
     def __init__(self, limite_pessoas):
         """Constructor for Banheiro"""
-        # variaaves
+        self.id_banheiro = Banheiro.id_banheiro
+        Banheiro.id_banheiro += 1
+
+        # controle de pessoas no banheiro
         self.limite_pessoas = limite_pessoas
         self.pessoas = 0
         self.vagas = BoundedSemaphore(value=self.limite_pessoas)
-        self.limite_swap = 2*limite_pessoas
-        self.swap_atual = 0
 
-        # variaaves
+        # situações do banheiro
+        self.disponivel = Event()
+        self.vazio = Event()
+        self.disponivel.set()
+        self.vazio.set()
+
+        # controle das filas do banheiro
+        self.pessoa_atual = None
+        self.fila = None
+
+        self.fila_masculino = Queue()
+        self.fila_feminino = Queue()
+
+        # controle de gênero do banheiro
         self.masculino = Event()
         self.feminino = Event()
-        # variaaves
-        self.tornar_unissex()
 
-    def swap(self):
-        self.swap_atual += 1
-        if self.swap_atual%self.limite_swap == 0:
-            self.swap_atual = 0
+        # banheiro começa masculino
+        self.tornar_masculino()
+
+        self.thread_fila = Thread(target=self.controlar_fila)
+        self.thread_fila.start()
+
+    def trocar_genero(self):
+        print_banheiro_log("Banheiro: " + str(self) + " vai trocar o gênero. Espera estar vazio!")
+        if not self.vazio.is_set():
+            self.vazio.wait()
+
+        if self.masculino.is_set():
+            self.tornar_feminino()
+        elif self.feminino.is_set():
+            self.tornar_masculino()
+
+    def controlar_fila(self):
+        while True:
             if self.masculino.is_set():
-                print_banheiro_log("Banheiro: foi masulino por muito tempo!!!!")
-                self.tornar_feminino()
-            if self.feminino.is_set():
-                print_banheiro_log("Banheiro: foi feminino por muito tempo!!!!")
-                self.tornar_masculino()
+                print_fila_log("fila: " + str(self) + " banheiro é masculino!")
+            elif self.feminino.is_set():
+                print_fila_log("fila: " + str(self) + " o banheiro é feminino!")
+            print_fila_log("fila: " + str(self) + " tem "+str(self.fila_masculino.qsize())+" homens na fila masculina!")
+            print_fila_log("fila: " + str(self) + " tem "+str(self.fila_feminino.qsize())+" mulheres na fila feminina!")
+
+            if (self.fila_masculino.qsize() > (self.fila_feminino.qsize()+self.limite_pessoas-(int(self.limite_pessoas/2))) or
+                    self.fila_feminino.qsize() > (self.fila_masculino.qsize()+self.limite_pessoas-(int(self.limite_pessoas/2))) or
+                    self.fila_feminino.empty() and not self.fila_masculino.empty() or
+                    self.fila_masculino.empty() and not self.fila_feminino.empty()):
+                self.trocar_genero()
+
+            if not self.disponivel.is_set():
+                print_fila_log("fila: " + str(self) + " espera estar disponível para liberar a fila!")
+                self.disponivel.wait()
+
+            self.pessoa_atual = self.fila.get()
+            print_fila_log("fila: " + str(self) + " é a vez da pessoa "+str(self.pessoa_atual))
+            self.pessoa_atual.vez.set()
+
+            print_fila_log("fila: " + str(self) + " espera a pessoa "+str(self.pessoa_atual)+" entrar no banheiro!")
+            if not self.pessoa_atual.entrou.is_set():
+                self.pessoa_atual.entrou.wait()
+
+            self.pessoa_atual.vez.clear()
 
     def tornar_feminino(self):
         self.feminino.set()
         self.masculino.clear()
-        print_banheiro_log("Banheiro: é feminino agora!")
+        self.fila = self.fila_feminino
+        print_banheiro_log("Banheiro: se tornou feminino agora!")
 
     def tornar_masculino(self):
         self.feminino.clear()
         self.masculino.set()
-        print_banheiro_log("Banheiro: é masculino agora!")
+        self.fila = self.fila_masculino
+        print_banheiro_log("Banheiro: se tornou masculino agora!")
 
-    def tornar_unissex(self):
-        self.masculino.set()
-        self.feminino.set()
-        print_banheiro_log("Banheiro: é unissex agora!")
+    def entrar_fila(self, pessoa):
+        if pessoa.sexo == 'M':
+            self.fila_masculino.put(pessoa)
+        elif pessoa.sexo == 'F':
+            self.fila_feminino.put(pessoa)
 
     def entrar(self):
-        if self.pessoas == 0:
-            self.swap_atual = 0
-            # if sexo == 'F':
-            #     self.feminino()
-            #     self.swap_atual = 0
-            # elif sexo == 'M':
-            #     self.masculino()
-            #     self.swap_atual = 0
+        # if self.pessoas == 0:
+        #     self.swap_atual = 0
 
         self.pessoas += 1
-
+        self.vazio.clear()
+        if self.pessoas == self.limite_pessoas:
+            self.disponivel.clear()
         print_banheiro_log("Banheiro: tem "+str(self.pessoas)+" pessoa(s) no banheiro")
 
-        self.swap()
+        # self.swap()
 
     def sair(self):
         self.pessoas -= 1
-
+        self.disponivel.set()
         print_banheiro_log("Banheiro: tem "+str(self.pessoas)+" pessoa(s) no banheiro")
 
         if self.pessoas == 0:
-            self.swap_atual = 0
-            self.tornar_unissex()
+            self.vazio.set()
+            # self.swap_atual = 0
+            # self.tornar_unissex()
+
+    def __str__(self):
+        return str(self.id_banheiro)
 
 
 class Pessoa(object):
     """Implementação de um banheiro"""
+
     id_pessoa = 1
+
     def __init__(self, sexo, banheiro):
         """Constructor for Pessoa"""
+
         self.id_pessoa = Pessoa.id_pessoa
         Pessoa.id_pessoa += 1
+
         self.sexo = sexo
         self.banheiro = banheiro
-        self.thread = Thread(target=self.run)
 
+        # controle de situações da pessoa
+        self.vez = Event()
+        self.entrou = Event()
+        self.vez.clear()
+        self.entrou.clear()
+
+        self.thread = Thread(target=self.run)
         self.thread.start()
 
     def run(self):
         while True:
             self.trabalhar()
-            self.esperar_genero()
+
+            print_pessoas_log("Pessoa: " + str(self) + " entra na fila!")
+            self.banheiro.entrar_fila(self)
+
             print_pessoas_log("Pessoa: " + str(self) + " pergunta: é minha vez?")
+            if not self.vez.is_set():
+                self.vez.wait()
+
+            print_pessoas_log("Pessoa: " + str(self) + " vai tentrar entrar no banheiro")
             with banheiro.vagas:
                 self.entrar()
                 self.sair()
@@ -151,22 +218,10 @@ class Pessoa(object):
         print_pessoas_log("Pessoa: "+str(self)+" vai trabalhar por "+str(tempo)+" segundos.")
         time.sleep(tempo)
 
-    def esperar_genero(self):
-        if self.sexo == 'M':
-            print_pessoas_log("Pessoa: " + str(self) + " pergunta: o banheiro é masculino?")
-            if not self.banheiro.masculino.is_set():
-                self.banheiro.masculino.wait()
-            self.banheiro.tornar_masculino()
-        elif self.sexo == 'F':
-            print_pessoas_log("Pessoa: " + str(self) + " pergunta: o banheiro é feminino?")
-            if not self.banheiro.feminino.is_set():
-                self.banheiro.feminino.wait()
-            self.banheiro.tornar_feminino()
-
     def entrar(self):
         print_pessoas_log("Pessoa: " + str(self) + " entrou no banheiro!")
         self.banheiro.entrar()
-
+        self.entrou.set()
         tempo = randrange(5) + 1
         print_pessoas_log("Pessoa: " + str(self) + " vai ficar no banheiro por " + str(tempo) + " segundos.")
         time.sleep(tempo)
@@ -174,6 +229,7 @@ class Pessoa(object):
     def sair(self):
         print_pessoas_log("Pessoa: "+str(self)+" saiu do banheiro")
         self.banheiro.sair()
+        self.entrou.clear()
 
     def __str__(self):
         return str(self.id_pessoa)+" ("+self.sexo+")"
